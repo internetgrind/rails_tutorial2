5ff066481355d297b3a9a6391d1435a0cf07ecd3
internetgrind <internetgrind[at]gmail[dot]com>
2012-09-18 01:50:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9cfeecd1740b4f55446b3a9f07d14a763b756dc5d1ce4b596dbaec1e19941538d880300b7291b0448c835bb595b64a3d8e4cd97e52c76bf61db59c4070c5df90'
